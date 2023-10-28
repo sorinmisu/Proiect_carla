7c98db21e056318aef2576a266f3075e0bcdc654
sorinmisu <sorinmisu@gmail.com>
2023-10-29 01:40:24 +0300

    chenar catel pozitionat

--- queryDb.js
+++ queryDb.js
@@ -16,26 +16,21 @@ const dbConfig = {
 // Define a route to serve the Catei.html page and Catei.css file
 app.use(express.static(__dirname + '/'));
 
+app.get('/getData', async (req, res) => {
+  try {
+    const connection = await mysql.createConnection(dbConfig);
+    const selectQuery = 'SELECT * FROM catei';
+    const [rows, fields] = await connection.execute(selectQuery);
+    connection.end();
 
-// app.get('/', (req, res) => {
-//     res.sendFile(__dirname + '/Catei.html');
-//   });
-  
-  app.get('/getData', async (req, res) => {
-    try {
-      const connection = await mysql.createConnection(dbConfig);
-      const selectQuery = 'SELECT * FROM useri';
-      const [rows, fields] = await connection.execute(selectQuery);
-      connection.end();
-  
-      // Send the data as a JSON response
-      res.json(rows);
-    } catch (error) {
-      console.error('Error selecting data:', error);
-      res.status(500).json({ error: 'An error occurred' });
-    }
-  });
-  
-  app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
-  });
+    // Send the data as a JSON response
+    res.json(rows);
+  } catch (error) {
+    console.error('Error selecting data:', error);
+    res.status(500).json({ error: 'An error occurred' });
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
