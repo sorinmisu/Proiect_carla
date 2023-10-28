8c557cb046988ee733741bafbdc2af73e19d0541
sorinmisu <sorinmisu@gmail.com>
2023-10-28 23:06:39 +0300

    start

---
+++ queryDb.js
@@ -0,0 +1,38 @@
+const mysql = require('mysql2/promise');
+const express = require('express')
+const app = express()
+const port = 3000
+
+// Replace these with your MariaDB connection details
+const dbConfig = {
+  host: 'localhost',
+  user: 'root',
+  password: 'root',
+  database: 'magazin',
+};
+
+
+
+// Define a route to serve the HTML page
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+  });
+  
+  app.get('/getData', async (req, res) => {
+    try {
+      const connection = await mysql.createConnection(dbConfig);
+      const selectQuery = 'SELECT * FROM useri';
+      const [rows, fields] = await connection.execute(selectQuery);
+      connection.end();
+  
+      // Send the data as a JSON response
+      res.json(rows);
+    } catch (error) {
+      console.error('Error selecting data:', error);
+      res.status(500).json({ error: 'An error occurred' });
+    }
+  });
+  
+  app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+  });
---
+++ server.js
@@ -0,0 +1,26 @@
+const http = require('http');
+const fs = require('fs');
+
+const server = http.createServer((req, res) => {
+  if (req.url === '/') {
+    // Serve the HTML file
+    fs.readFile('index.html', (err, data) => {
+      if (err) {
+        res.writeHead(500, { 'Content-Type': 'text/plain' });
+        res.end('Internal Server Error');
+      } else {
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.end(data);
+      }
+    });
+  } else {
+    // Handle other requests (e.g., API endpoints)
+    res.writeHead(404, { 'Content-Type': 'text/plain' });
+    res.end('Not Found');
+  }
+});
+
+const port = 3000;
+server.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
