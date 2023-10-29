bc2e83036f06bfdb951482980fe1c84da80c1e10
sorinmisu <sorinmisu@gmail.com>
2023-10-29 16:42:59 +0200

    bkp

--- queryDb.js
+++ queryDb.js
@@ -2,6 +2,7 @@ const mysql = require('mysql2/promise');
 const express = require('express')
 const app = express()
 const port = 3000
+app.use(express.json());
 
 // Replace these with your MariaDB connection details
 const dbConfig = {
@@ -30,6 +31,38 @@ app.get('/getData', async (req, res) => {
     res.status(500).json({ error: 'An error occurred' });
   }
 });
+
+
+app.delete('/deleteData/:id', async (req, res) => {
+  try {
+    const connection = await mysql.createConnection(dbConfig);
+    const deleteQuery = 'DELETE FROM catei WHERE id = ?';
+    const [result] = await connection.execute(deleteQuery, [req.params.id]);
+    connection.end();
+
+    if (result.affectedRows === 0) {
+      // If no rows were affected, the ID was not found
+      res.status(404).json({ error: 'ID not found' });
+    } else {
+      // Send a success response
+      res.json({ success: true });
+    }
+  } catch (error) {
+    console.error('Error deleting data:', error);
+    alert(`Failed to delete entry: ${error.message}`);
+    res.status(500).json({ error: 'An error occurred' });
+  }
+});
+
+app.put('/updateData/:id', async (req, res) => {
+  //This function is called from a html button event
+  // i want to print in the console the headers of the PUT method
+  console.log(req.body);
+
+
+    
+});
+
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
